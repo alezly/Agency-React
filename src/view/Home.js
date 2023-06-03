be19f4895053e3a72945741cf99d7e9d415a9985
alezly <albanyramirez8@gmail.com>
2023-06-02 22:55:37 -0400

    first commit

---
+++ src/view/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Home() {
+    return(
+        <div className='Container'>
+            <h1 className='textAnimation'>Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative-Creative</h1>
+        </div>
+    )
+}
+
+export default Home;
